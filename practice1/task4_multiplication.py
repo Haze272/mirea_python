c8c39800f1222b15ac795ee6aa2cbadb170f643f
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2022-02-12 22:12:35 +0300

    Initital commit

---
+++ practice1/task4_multiplication.py
@@ -0,0 +1,14 @@
+def multi12(num):
+    num += num
+    num += num
+    num += num + num
+    return num
+
+def multi16(num):
+    num += num
+    num += num
+    num += num
+    num += num
+    return num
+
+print(multi16(3))
