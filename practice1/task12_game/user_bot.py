69b60c9ed83b008b074502125ae951a7c450841a
Stepan <studiorain27@gmail.com>
2022-02-19 22:39:32 +0300

    Found out that near_gold() is updating if you catch the gold

--- practice1/task12_game/user_bot.py
+++ practice1/task12_game/user_bot.py
@@ -1,7 +1,4 @@
-﻿from queue import PriorityQueue
-
-import numpy as np
-from numpy import linalg as LA
+﻿import numpy as np
 
 class Coordinate():
     def __init__(self, x, y):
@@ -27,7 +24,140 @@ def near_gold(check, x, y, level):
                 gold_dict[heuristic(Coordinate(x, y), Coordinate(i, j))] = Coordinate(i, j)
     sorted_dict = {k: gold_dict[k] for k in sorted(gold_dict)}
     keys = list(sorted_dict.keys())
+
+    print("Near gold is in", sorted_dict[keys[0]].x, sorted_dict[keys[0]].y)
+
     return sorted_dict[keys[0]]
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
+def get_destination_to(check, x1, y1, x2, y2):
+    if x1 == x2 and y1 == y2:
+        print("координаты одинаковы", x1, y1, x2, y2)
+    wall_dict = np.zeros((28, 25))
+
+    for i in range(28):
+        for j in range(25):
+            if check("wall", i, j) == True:
+                wall_dict[i][j] = 1
+
+    wall_dict = wall_dict.transpose()
+
+    start = (y1, x1)
+    end = (y2, x2)
+
+    path = astar(wall_dict, start, end)
+    print(path)
+    print('\n')
+    print("NIGGERS", x1, y1)
+    if path[1][0] - path[0][0] == 1:
+        return "down"
+    elif path[1][0] - path[0][0] == -1:
+        return "up"
+    elif path[1][1] - path[0][1] == 1:
+        return "right"
+    elif path[1][1] - path[0][1] == -1:
+        return "left"
+    print("NIGGERS")
+    return "up"
+
+
+
 
 def script(check, x, y):
     if check("level") == 1:
@@ -40,10 +170,8 @@ def script(check, x, y):
 
     elif check("level") == 2:
 
-        gold = near_gold(check, x, y, 2) # [1; 6]
-        print(gold.x, gold.y)
+        gold = near_gold(check, x, y, 2)
 
-        #heuristic(Coordinate(x, y), Coordinate(gold.x, gold.y))
         if check("gold", x, y):
             return "take"
         if x < gold.x:
@@ -57,31 +185,14 @@ def script(check, x, y):
 
     elif check("level") == 3:
 
-        if x == 1 and y == 23:
-            wall_dict = np.zeros((28, 25))
+        if check("gold", x, y):
+            return "take"
+        print('\n')
 
-            for i in range(28):
-                for j in range(25):
-                    if check("wall", i, j) == True:
-                        wall_dict[i][j] = 1
-            wall_dict[1][23] = 4
+        depart = Coordinate(x, y)
+        fini = near_gold(check, depart.x, depart.y, 3)
 
-            wall_dict = wall_dict.transpose()
-            print(wall_dict)
-            print('\n')
-
-
-            path_dict = np.zeros((28, 25))
-            path_dict[1, 23] = 8
-            depart = Coordinate(1, 23)
-            fini = near_gold(check, depart.x, depart.y, 3)
-
-
-
-            path_dict = path_dict.transpose()
-            print(path_dict)
-
-
-
-        return "up"
-    return "pass"
+        destination = get_destination_to(check, depart.x, depart.y, fini.x, fini.y)
+        print(destination)
+        return destination
+    return "up"
