c7ec5e552317a6b334dfffccd6f013b885638f48
Stepan <studiorain27@gmail.com>
2022-03-15 10:13:12 +0300

    Done kispython task1

--- kispython/task1.py
+++ kispython/task1.py
@@ -1,17 +1,28 @@
 import math
 
 
-def main(z):
-    if pow(z, 3) < 0:
-        zerodiv = math.ceil(-(pow(z, 3)))
-    else:
-        zerodiv = math.ceil(pow(z, 3))
-    num1 = (((z + 1) * (z + 1)) - 96 * ((z * z / 29) - (94 * z * z * z)))
-    denum1 = pow(zerodiv, 5)
-    num2 = pow((z * z - 1), 4)
-    denum2 = (36 - (77 * pow((29 * z * z * z), 4)))
-    result = round((num1/denum1 + num2/denum2), 2)
+def int_r(num):
+    num = int(num + (0.5 if num > 0 else -0.5))
+    return num
 
-    return abs(result)
+
+def main(z):
+    step1 = pow(z + 1, 2)
+    step2 = 96 * (pow(z, 2) / 29 - 94 * pow(z, 3))
+    result = step1 - step2
+
+    step3 = pow(z, 3)
+    if step3 < 0:
+        step3 = -math.ceil(-step3)
+    step4 = pow(step3, 5)
+    result /= step4
+
+    step5 = pow(pow(z, 2) - 1, 4)
+    step6 = 36 - 77 * pow(29 * pow(z, 3), 4)
+    step7 = step5 / step6
+    result += step7
+
+    return result
 
 print(main(-0.95))
+print(main(-0.75))
