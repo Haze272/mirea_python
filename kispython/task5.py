abbd1422978c4e2296b4335d8f6c54f863a9e806
Stepan <studiorain27@gmail.com>
2022-03-19 16:24:18 +0300

    Update task5.py

--- kispython/task5.py
+++ kispython/task5.py
@@ -4,11 +4,12 @@ import math
 def main(z):
     sum = 0
     for i in range(1, len(z) + 1):
-        num1 = pow(z[math.ceil(i/3)], 3)
-        num2 = 8 * pow(z[math.ceil(i/3)], 2)
-        sum += (num1 - num2)**2
+        num1 = pow(z[math.ceil(i / 3)], 3)
+        num2 = 8 * pow(z[math.ceil(i / 3)], 2)
+        sum += (num1 - num2) ** 2
 
     return sum * 66
+
 
 arr = [0.35, -0.93, 0.94, 0.59, 0.05, 0.75, -0.52]
 print(main(arr))
